ab5ba858f97c8082e7a86ace645ad0b656720f5d
Naveen <naveen@naveens-MacBook-Pro.local>
2020-03-25 16:16:02 +0530

    first commit

---
+++ ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/naveen/Development/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/naveen/AndroidStudioProjects/flutter_socket_io/example"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_FRAMEWORK_DIR=/Users/naveen/Development/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
